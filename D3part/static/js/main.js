20ce8d414c20e7d5ce32b01563a55f67d3d4cd1c
Isabelle Hallman <isaha894@student.liu.se>
2018-05-04 10:03:51 +0200

    started flowmap

--- D3part/static/js/main.js
+++ D3part/static/js/main.js
@@ -2,9 +2,9 @@
 var map, pc, bc;
 function draw(activeData, dFri, dSat, dSun){
   
-  map = new Map(data2014, data2010, data2006, data2002, pcYear, sweden_map_json);
-  pc = new pc(pcYear);
-  bc = new bc(pcYear);
+  //map = new Map(data2014, data2010, data2006, data2002, pcYear, sweden_map_json);
+  //pc = new pc(pcYear);
+  //bc = new bc(pcYear);
 }
 
 d3.queue()
