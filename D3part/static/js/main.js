628f8f9a797031c50637f355597926a34676fa72
Isabelle Hallman <isaha894@student.liu.se>
2018-05-04 08:51:08 +0200

    started adjusting d3 project to fit this project

--- D3part/static/js/main.js
+++ D3part/static/js/main.js
@@ -1,6 +1,6 @@
 
 var map, pc, bc;
-function draw(activeData, data2014, data2010, data2006, data2002, pcYear, sweden_map_json){
+function draw(activeData, dFri, dSat, dSun){
   
   map = new Map(data2014, data2010, data2006, data2002, pcYear, sweden_map_json);
   pc = new pc(pcYear);
@@ -8,16 +8,13 @@ function draw(activeData, data2014, data2010, data2006, data2002, pcYear, sweden
 }
 
 d3.queue()
-    .defer(d3.csv, 'static/data/Swedish_Election_2014.csv')
-    .defer(d3.csv, 'static/data/Swedish_Election_2010.csv')
-    .defer(d3.csv, 'static/data/Swedish_Election_2006.csv')
-    .defer(d3.csv, 'static/data/Swedish_Election_2002.csv')
-    .defer(d3.csv, 'static/data/pcYear.csv')                  //#1
-    .defer(d3.json,'static/maps/sverige-topo.json')
-    .await(function(error, d2014, d2010, d2006, d2002, pcYear, sweden_map_json) {
+    .defer(d3.csv, 'static/data/comm-data-Fri.csv')
+    .defer(d3.csv, 'static/data/comm-data-Sat.csv')
+    .defer(d3.csv, 'static/data/comm-data-Sun.csv')
+    .await(function(error, dFri, dSat, dSun) {
       if (error) throw error;  
      
-      draw(d2014, d2014, d2010, d2006, d2002, pcYear, sweden_map_json);
+      draw(dFri, dFri, dSat, dSun);
     });
 
 
