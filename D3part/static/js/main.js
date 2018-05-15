d38aed4ca8f22d34d89e5ebe08f9ccbbb4e94cfe
Axel Kollberg <bollbergo@gmail.com>
2018-05-15 08:21:54 +0200

    Added a index-file, containing the d3 code for creating a force directed graph

--- D3part/static/js/main.js
+++ D3part/static/js/main.js
@@ -1,16 +1,17 @@
 
 var map, pc, bc;
-function draw(activeData, data2014, data2010, data2006, data2002, pcYear, sweden_map_json){
+function draw(activeData, data2014, data2010, data2006, commData, data2002, pcYear, sweden_map_json){
   
-  map = new Map(data2014, data2010, data2006, data2002, pcYear, sweden_map_json);
+  map = new Map(data2014, data2010, data2006, commData, data2002, pcYear, sweden_map_json);
   pc = new pc(pcYear);
-  bc = new bc(pcYear);
+  bc = new bc(commData);
 }
 
 d3.queue()
     .defer(d3.csv, 'static/data/Swedish_Election_2014.csv')
     .defer(d3.csv, 'static/data/Swedish_Election_2010.csv')
     .defer(d3.csv, 'static/data/Swedish_Election_2006.csv')
+    .defer(d3.csv, 'static/data/comm-data-Fri.csv')
     .defer(d3.csv, 'static/data/Swedish_Election_2002.csv')
     .defer(d3.csv, 'static/data/pcYear.csv')                  //#1
     .defer(d3.json,'static/maps/sverige-topo.json')
