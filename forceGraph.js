ab986ac6d56f334449cd91d25b0e6e31c63694e8
Axel Kollberg <bollbergo@gmail.com>
2018-05-20 16:16:31 +0200

    Added filtering and selection to the forcegraph

--- forceGraph.js
+++ forceGraph.js
@@ -2,6 +2,9 @@ function forceGraph(data) {
 
     this.data = data;
     var listToSend = [];
+    let allComms = [];
+    let allToComms = [];
+    let locations = [];
 
     var svg = d3.select("svg"),
     margin = {right: 50, left: 50}, 
@@ -17,52 +20,93 @@ function forceGraph(data) {
         }))
         .append("g")
 
-    update(0);
+    data.forEach(function(d) {
+        allComms.push(parseInt(d.from));
+        allToComms.push(parseInt(d.to));
+        locations.push(d.location)
+    })
 
-    function update(startVal)
+    update(0, -1);
+
+    function update(startVal, filteredID)
     {
         const minVal = Math.min(data.length-1,startVal+Math.floor((data.length / 200))); 
         document.getElementById("output").value = data[Math.max(Math.floor(((data.length / 200) * (document.getElementById("slider").value))) -1,0)].Timestamp + " -> " + data[minVal].Timestamp;
         svg.selectAll("*").remove();
 
+        let filteredIds = filteredID;
 
-        let allComms = [];
-        let allToComms = [];
         let links = {};
         let unique = [];
         let listOfLinks = [];
-        let locations = [];
         let start = Math.floor(startVal);
-    
-        let num = data.length/200;
-    
-        // Fetching necessary data and storing it in local variables
-        data.forEach(function(d) {
-        allComms.push(parseInt(d.from));
-        allToComms.push(parseInt(d.to));
-        locations.push(d.location)
-        })
+        let num;
+
+        if(filteredID < 0)
+            num = data.length/200;
+        else
+            num = data.length;
     
     
         // Just taking a limited number of the dataset, to improve perfomance and interactability
-        for(var i = start; i < Math.min(data.length,(start +num)); i++){
-            if(unique.indexOf(allComms[i]) === -1){
-                unique.push(allComms[i]);
-            }
-            if(unique.indexOf(allToComms[i]) === -1) {
-            unique.push(allToComms[i]);
+        if(filteredID < 0) {
+            for(var i = start; i < Math.min(data.length,(start +num)); i++){
+                if(unique.indexOf(allComms[i]) === -1){
+                    unique.push(allComms[i]);
+                }
+                if(unique.indexOf(allToComms[i]) === -1) {
+                    unique.push(allToComms[i]);
+                }
             }
         }
-    
+        else {
+            for(var i = 0; i < num; i++){
+                if(filteredIds.indexOf(allComms[i]) != -1 && unique.indexOf(allToComms[i]) === -1){
+                    unique.push(allToComms[i]);
+                }
+                if(filteredIds.indexOf(allToComms[i]) != -1 && unique.indexOf(allComms[i]) === -1) {
+                    unique.push(allComms[i]);
+                }
+            }
+            for(var i = 0; i < filteredIds.length; i++){
+                if(unique.indexOf(filteredIds[i]) === -1)
+                    unique.push(filteredIds[i]);
+            }
+            console.log(unique);
+        } 
+
         // Initializing a JSON-formatted object with an array of all communcation sources to target.
-        for(var k = start; k < Math.min(data.length,(start +num)); k++){
-            let tinyObj2 = {};
-            tinyObj2.source = allComms[k];
-            tinyObj2.target = allToComms[k];
-            tinyObj2.value = 1;
-            tinyObj2.location = locations[k];
-            listOfLinks.push(tinyObj2);
+        if(filteredID < 0){
+            for(var k = start; k < Math.min(data.length,(start +num)); k++){
+                let tinyObj2 = {};
+                tinyObj2.source = allComms[k];
+                tinyObj2.target = allToComms[k];
+                tinyObj2.value = 1;
+                tinyObj2.location = locations[k];
+                listOfLinks.push(tinyObj2);
+            }
         }
+        else{
+            for(var k = 0; k < num; k++){
+                if(filteredIds.indexOf(allComms[k]) != -1){
+                    let tinyObj2 = {};
+                    tinyObj2.source = allComms[k];
+                    tinyObj2.target = allToComms[k];
+                    tinyObj2.value = 1;
+                    tinyObj2.location = locations[k];
+                    listOfLinks.push(tinyObj2);
+                }
+
+                if(filteredIds.indexOf(allToComms[k]) != -1){
+                    let tinyObj2 = {};
+                    tinyObj2.source = allComms[k];
+                    tinyObj2.target = allToComms[k];
+                    tinyObj2.value = 1;
+                    tinyObj2.location = locations[k];
+                    listOfLinks.push(tinyObj2);
+                }
+            }
+        }   
         
         let listOfGroups = [];
         let j = 0;
@@ -73,14 +117,14 @@ function forceGraph(data) {
             j++;
             listOfGroups.push(tinyObj);
         })
-        listOfGroups.push({"id": "0", "group": 2950});
+        console.log(listOfGroups);
         uniqObj = listOfGroups;
         
         var link = svg.append("g")
             .attr("class", "links")
-        .selectAll("line")
-        .data(listOfLinks)
-        .enter().append("line")
+            .selectAll("line")
+            .data(listOfLinks)
+            .enter().append("line")
             .attr("stroke-width", function(d) { return d.value;});
     
 
@@ -92,7 +136,7 @@ function forceGraph(data) {
             .attr("r", 5)
             .style("stroke", "gray")
             .style("stroke-width", "0.5px")
-            .attr("fill", function(d) { return colors[getColorIndex(d.id, listOfLinks)];})
+            .attr("fill", function(d) { if(listToSend.indexOf(d.id) != -1){return "blue";}else return colors[getColorIndex(d.id, listOfLinks)];})
             .call(d3.drag()
             .on("start", dragstarted)
             .on("drag", dragged)
@@ -124,13 +168,12 @@ function forceGraph(data) {
 
         function arrayBuilder(id){
 
-
-            if(listToSend.indexOf(id.id.toString()) == -1){
-                listToSend.push(id.id.toString());
+            if(listToSend.indexOf(id.id) == -1){
+                listToSend.push(id.id);
                 d3.select(this).style("fill", "blue");
             }
             else{
-                listToSend.splice(listToSend.indexOf(id.id.toString()),1);
+                listToSend.splice(listToSend.indexOf(id.id),1);
                 d3.select(this).style("fill", function(d) {return colors[getColorIndex(id, listOfLinks)]});
             }
             console.log(listToSend);
@@ -140,8 +183,14 @@ function forceGraph(data) {
     
     this.updateForceGraph = function() {
         var someVal = Math.floor(((data.length / 200) * document.getElementById("slider").value));
-        update(someVal);
+        update(someVal, -1);
     }
+    this.filterForceData = function() {
+        console.log("Starting filtering.")
+        update(0, listToSend);
+        document.getElementById("output").value = "Displaying commdata from: " + listToSend;
+    }
+
 
 
 
